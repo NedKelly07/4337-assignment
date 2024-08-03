9ac7aa990f3db2249905ccdde72a6bc15fa4197b
Ben <bcrabbers55@gmail.com>
2024-08-03 14:13:39 +1000

    Commented out Task 1-4 messages to help show other ones

--- src/DIMYServer.py
+++ src/DIMYServer.py
@@ -63,7 +63,6 @@ def start():
 
 def main():
     print(f"[SERVER STARTUP] - binding to {address}")
-    print(f'debug mode is set to [{debug}], this controlls the extra verbose '+ '\033[1;90m' + 'grey output' + '\033[0m' + ', toggle this at the top of helper.py')
     start()
 
 if __name__ == "__main__":
